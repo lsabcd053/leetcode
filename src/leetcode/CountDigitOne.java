1537a87205510f59139bd03dfb3ec1bd67c13bff
lsabcdgit <lidingbo_1989@163.com>
2015-07-09 23:38:08 +0800

    -

---
+++ src/leetcode/CountDigitOne.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+public class CountDigitOne {
+	public static void main(String []argv){
+		
+	}
+	public int countDigitOne(int n) {
+        int res =0;
+        return res;
+    }
+}
